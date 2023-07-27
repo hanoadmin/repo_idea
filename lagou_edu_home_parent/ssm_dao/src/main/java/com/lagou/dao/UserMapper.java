f3b014bddf5e888ec589f7a57c1327f8e414a2c1
repo_idea <140515134+hanoadmin@users.noreply.github.com>
2023-07-27 22:41:06 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -52,5 +52,6 @@ public interface UserMapper {
      */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
 
+    public List<Resource> findResourceByRoleId2(List<Integer> ids);
 
 }
